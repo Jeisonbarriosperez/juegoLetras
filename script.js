5a0c2bfd7d0d4ddfff520d74df968eee2c272d18
Jeisonbarriosperez <jeison.barrios@cecar.edu.co>
2023-08-28 18:41:49 -0500

    juego letras

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-const wordE1 = document.getElementById('word');
-const wrongLettersE1 = document.getElementById('wrong-letters');
-const playAgainBtn = document.getElementById('play-button');
-const popup = document.getElementById('popup-container');
-const notification = document.getElementById('notification-container');
-const finalMessage = document.getElementById('final-message');
-
+const wordE1 = document.getElementById("word");
+const wrongLettersE1 = document.getElementById("wrong-letters");
+const playAgainBtn = document.getElementById("play-button");
+const popup = document.getElementById("popup-container");
+const notification = document.getElementById("notification-container");
+const finalMessage = document.getElementById("final-message");
 const figureParts = document.querySelectorAll(".figure-part");
-
 const words = ['perro', 'gato', 'elefante', 'cebra', 'rana', 'león', 'tigre',
     'conejo', 'jirafa', 'hipopótamo', 'tortuga', 'oso', 'camello', 'mono', 'koala',
     'panda', 'puercoespín', 'ardilla', 'castor', 'búho', 'águila', 'colibrí',
@@ -43,110 +41,106 @@ const words = ['perro', 'gato', 'elefante', 'cebra', 'rana', 'león', 'tigre',
     'tergiversar', 'magnífico', 'fotográfico', 'puntualidad', 'reivindicar',
     'descomunal', 'estadístico', 'comunicación', 'transatlántico', 'microorganismo',];
 
-
 let selectedWord = words[Math.floor(Math.random() * words.length)];
-
 const correctLetters = [];
 const wrongLetters = [];
 
 //Show hidden word
 function displayWord() {
-    wordE1.innerHTML = `
+  wordE1.innerHTML = `
     ${selectedWord
-            .split('')
-            .map(
-                letter => `
+      .split("")
+      .map(
+        (letter) => `
         <span class="letter">
-        ${correctLetters.includes(letter) ? letter : ''}
+        ${correctLetters.includes(letter) ? letter : ""}
         </span>
         `
-            )
-            .join('')}
+      )
+      .join("")}
     `;
 
-    const innerWord = wordE1.innerText.replace(/\n/g, '');
+  const innerWord = wordE1.innerText.replace(/\n/g, "");
 
-    if (innerWord === selectedWord) {
-        finalMessage.innerText = '🥳¡Felicidades! ¡Ganaste!😃';
-        popup.style.display = 'flex';
-    }
+  if (innerWord === selectedWord) {
+    finalMessage.innerText = "🥳¡Felicidades! ¡Ganaste!😃";
+    popup.style.display = "flex";
+  }
 }
 
 // Update the wrong letters
 function updateWrongLetterE1() {
-    //Display wrong letters
-    wrongLettersE1.innerHTML = `
-    ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
-    ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+  //Display wrong letters
+  wrongLettersE1.innerHTML = `
+    ${wrongLetters.length > 0 ? "<p>Wrong</p>" : ""}
+    ${wrongLetters.map((letter) => `<span>${letter}</span>`)}
     `;
 
-    //Display parts
-    figureParts.forEach((part, index) => {
-        const errors = wrongLetters.length;
+  //Display parts
+  figureParts.forEach((part, index) => {
+    const errors = wrongLetters.length;
 
-        if (index < errors) {
-            part.style.display = 'block'
-        }
-        else {
-            part.style.display = 'none';
-        }
-    });
-
-    //Check if lost
-    if (wrongLetters.length === figureParts.length) {
-        finalMessage.innerText = ' 😱 Lamentablemente perdiste.😕';
-        popup.style.display = 'flex';
+    if (index < errors) {
+      part.style.display = "block";
+    } else {
+      part.style.display = "none";
     }
+  });
+
+  //Check if lost
+  if (wrongLetters.length === figureParts.length) {
+    finalMessage.innerText = " 😱 Lamentablemente perdiste.😕";
+    popup.style.display = "flex";
+  }
 }
 
 //Show notification
 function showNotification() {
-    notification.classList.add('show');
+  notification.classList.add("show");
 
-    setTimeout(() => {
-        notification.classList.remove('show');
-    }, 2000);
+  setTimeout(() => {
+    notification.classList.remove("show");
+  }, 2000);
 }
 
 //Keydown letter press
-window.addEventListener('keydown', e => {
-    if (e.keyCode >= 65 && e.keyCode <= 90) {
-        const letter = e.key;
+window.addEventListener("keydown", (e) => {
+  if (e.key >= 65 && e.key <= 90) {
+    const letter = e.key;
 
-        if (selectedWord.includes(letter)) {
-            if (!correctLetters.includes(letter)) {
-                correctLetters.push(letter);
+    if (selectedWord.includes(letter)) {
+      if (!correctLetters.includes(letter)) {
+        correctLetters.push(letter);
 
-                displayWord();
-            } else {
-                showNotification();
-            }
-        } else {
-            if (!wrongLetters.includes(letter)) {
-                wrongLetters.push(letter);
+        displayWord();
+      } else {
+        showNotification();
+      }
+    } else {
+      if (!wrongLetters.includes(letter)) {
+        wrongLetters.push(letter);
 
-                updateWrongLetterE1();
-            } else {
-                showNotification();
-            }
-        }
+        updateWrongLetterE1();
+      } else {
+        showNotification();
+      }
     }
+  }
 });
 
 //Restart game and play again
-playAgainBtn.addEventListener('click', () => {
-    //Empty arrays
-    correctLetters.splice(0);
-    wrongLetters.splice(0);
+playAgainBtn.addEventListener("click", () => {
+  //Empty arrays
+  correctLetters.splice(0);
+  wrongLetters.splice(0);
 
-    selectedWord = words[Math.floor(Math.random() * words.length)];
+  selectedWord = words[Math.floor(Math.random() * words.length)];
 
-    displayWord();
+  displayWord();
 
-    updateWrongLetterE1();
+  updateWrongLetterE1();
 
-    popup.style.display = 'none';
+  popup.style.display = "none";
 });
 
 displayWord();
-
